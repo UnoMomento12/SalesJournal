7a0722b937407375488c34ff6960ee72bf4c1d57
Kyrylo <kyryloyurchenkoaccs@gmail.com>
2020-04-16 11:06:32 +0300

    start

---
+++ Controls/ImageButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalesJournal
+{
+    /// <summary>
+    /// Interaction logic for ImageButton.xaml
+    /// </summary>
+    public partial class ImageButton : Control
+    {
+        static ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton),
+                new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(default(ImageSource)));
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using SalesJournal.Pages;
+
+namespace SalesJournal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Pages And Frames
+        public Frame       salesFrame;
+        public SalesPage   salesPage;
+        public Frame       ordersFrame;
+        public OrdersPage  ordersPage;
+        public Frame       archiveFrame;
+        public ArchivePage archivePage;
+        public Frame       stockFrame;
+        public StockPage   stockPage;
+        public Frame       statsFrame;
+        public StatsPage   statsPage;
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+            BindPagesToTabs();
+            
+        }
+        
+        public void BindPagesToTabs()
+        {
+            Thickness plugC = new Thickness(0);
+            
+
+            salesFrame = new Frame();
+            ordersFrame = new Frame(); 
+            archiveFrame = new Frame(); 
+            stockFrame = new Frame();
+            statsFrame = new Frame();
+
+            salesPage = new SalesPage();
+            ordersPage = new OrdersPage();
+            archivePage = new ArchivePage();
+            stockPage = new StockPage();
+            statsPage = new StatsPage();
+
+            salesFrame.Content = salesPage;
+            SalesTab.Content = salesFrame;
+            ordersFrame.Content = ordersPage;
+            OrdersTab.Content = ordersFrame;
+            statsFrame.Content = statsPage;
+            StatsTab.Content = statsFrame;
+
+            stockFrame.Content = stockPage;
+            StockTab.Content = stockFrame;
+            archiveFrame.Content = archivePage;
+            ArchiveTab.Content = archiveFrame;
+            
+        }
+    }
+}
